b896f1be7b33bf391502bf9b47d5f2d767a3ebb5
ChiragRBhavsar <69681103+ChiragRBhavsar@users.noreply.github.com>
2020-09-18 12:36:35 +0530

    Update Sim.c

--- Sim.c
+++ Sim.c
@@ -6,4 +6,10 @@ int principle;
 float Time;
 int simpleinterest;
 float d = simpleinterest;
-Rate
+Rate = 10;
+principle = 20000;
+Time = 1;
+d = (principle*Rate*Time)/100;
+printf("The Value of Rate is %d\n",Rate);
+printf("The value of principle is %d\n",principle);
+printf("The value of Time is %.1f\n",Time);
